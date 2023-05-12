56341700bf4e33c4afc428eb0dc67c9d70c05856
Kundan-10 <kundanwakudkar10@gmail.com>
2023-05-12 19:44:28 +0530

    customerService impl added in service

--- SpringSecurityAuthProviderWithoutJwt-1/src/main/java/com/spring/service/CustomerService.java
+++ SpringSecurityAuthProviderWithoutJwt-1/src/main/java/com/spring/service/CustomerService.java
@@ -1,5 +1,18 @@
 package com.spring.service;
 
+import java.util.List;
+
+import com.spring.exception.CustomerException;
+import com.spring.model.Customer;
+
 public interface CustomerService {
+	
+    public Customer registerCustomer(Customer customer);
+	
+	public Customer getCustomerDetailsByEmail(String email)throws CustomerException;
+	
+	public List<Customer> getAllCustomerDetails()throws CustomerException;
+
+	
 
 }
---
+++ SpringSecurityAuthProviderWithoutJwt-1/src/main/java/com/spring/service/CustomerServiceImpl.java
@@ -0,0 +1,45 @@
+package com.spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring.exception.CustomerException;
+import com.spring.model.Customer;
+import com.spring.repository.CustomerRepository;
+
+public class CustomerServiceImpl implements CustomerService{
+	
+	@Autowired
+	private CustomerRepository customerRepo;
+
+	@Override
+	public Customer registerCustomer(Customer customer) {
+		
+		return customerRepo.save(customer);
+
+	}
+
+	@Override
+	public Customer getCustomerDetailsByEmail(String email) throws CustomerException {
+		
+		return customerRepo.findByEmail(email)
+				.orElseThrow(() -> new CustomerException("Customer Not found with Email: "+email));	
+
+	}
+
+	@Override
+	public List<Customer> getAllCustomerDetails() throws CustomerException {
+
+		
+			
+			List<Customer> customers= customerRepo.findAll();
+			
+			if(customers.isEmpty())
+				throw new CustomerException("No Customer find");
+			
+			return customers;
+	}
+	
+
+}
