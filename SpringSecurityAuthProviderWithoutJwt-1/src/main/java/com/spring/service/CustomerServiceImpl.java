24a0bdb5c68d03dbc07b6ae622389d44ba34cc8b
Kundan-10 <kundanwakudkar10@gmail.com>
2023-05-12 23:44:36 +0530

    saveCusotmer handler added in Controller

--- SpringSecurityAuthProviderWithoutJwt-1/src/main/java/com/spring/service/CustomerServiceImpl.java
+++ SpringSecurityAuthProviderWithoutJwt-1/src/main/java/com/spring/service/CustomerServiceImpl.java
@@ -3,11 +3,13 @@ package com.spring.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.spring.exception.CustomerException;
 import com.spring.model.Customer;
 import com.spring.repository.CustomerRepository;
 
+@Service
 public class CustomerServiceImpl implements CustomerService{
 	
 	@Autowired
