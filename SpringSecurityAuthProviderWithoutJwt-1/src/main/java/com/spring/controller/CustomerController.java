ff0eaef7d2beea9c5592bde30ec087980c29f9b3
Kundan-10 <kundanwakudkar10@gmail.com>
2023-05-12 23:47:06 +0530

    getCustomerByEmailHandler added

--- SpringSecurityAuthProviderWithoutJwt-1/src/main/java/com/spring/controller/CustomerController.java
+++ SpringSecurityAuthProviderWithoutJwt-1/src/main/java/com/spring/controller/CustomerController.java
@@ -52,15 +52,7 @@ public class CustomerController {
 		
 	}
 	
-	@GetMapping("/customers")
-	public ResponseEntity<List<Customer>> getAllCustomerHandler() throws CustomerException{
-		
-		
-		List<Customer> customers= customerService.getAllCustomerDetails();
-		
-		return new ResponseEntity<>(customers,HttpStatus.ACCEPTED);
-		
-	}
+
 	
 	
 
